15430aa8aa0ec37d4156ed72295b88cc8b7c331f
shinichi kogai <kogai0121@gmail.com>
2021-08-16 14:16:55 +0900

    introduce session less token

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,44 @@
-import { Heading, Page } from "@shopify/polaris";
+import { useEffect } from "react";
+import { Heading, Page, Button, Card, List } from "@shopify/polaris";
+import { useQuery, useLazyQuery } from "react-apollo";
+import gql from "graphql-tag";
 
-const Index = () => (
-  <Page>
-    <Heading>Shopify app with Node and React 🎉</Heading>
-  </Page>
-);
+const Index = () => {
+  const [getShop, { data, error, loading }] = useLazyQuery(
+    gql`
+      query {
+        shop {
+          name
+          email
+          description
+          myshopifyDomain
+          primaryDomain {
+            sslEnabled
+            url
+          }
+        }
+      }
+    `
+  );
+  console.log(loading, data, error);
+  return (
+    <Page>
+      <Heading>Shopify app with Node and React 🎉</Heading>
+      <Card
+        title="ストアの情報"
+        primaryFooterAction={{
+          content: "ストアの情報を取得する",
+          onAction: getShop,
+        }}
+      >
+        <Card.Section title="取得結果">
+          <List>
+            <List.Item>{data?.shop?.name || "未取得"}</List.Item>
+          </List>
+        </Card.Section>
+      </Card>
+    </Page>
+  );
+};
 
 export default Index;
--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ Shopify.Context.initialize({
   API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
   SCOPES: process.env.SCOPES.split(","),
   HOST_NAME: process.env.HOST.replace(/https:\/\//, ""),
-  API_VERSION: ApiVersion.October20,
+  API_VERSION: ApiVersion.April21,
   IS_EMBEDDED_APP: true,
   // This should be replaced with your preferred storage strategy
   SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
@@ -80,6 +80,7 @@ app.prepare().then(async () => {
 
   router.post(
     "/graphql",
+    // NOTE: https://github.com/Shopify/koa-shopify-auth/blob/master/src/verify-request/verify-token.ts
     verifyRequest({ returnHeader: true }),
     async (ctx, next) => {
       await Shopify.Utils.graphqlProxy(ctx.req, ctx.res);
